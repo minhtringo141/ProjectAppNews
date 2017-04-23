4c66f9f99604802efb838d09e8871c5a8fa01481
MinhTri Ngo <minhtringo141@gmail.com>
2017-04-23 22:24:08 +0700

    fix format json

--- api/items/item.controller.js
+++ api/items/item.controller.js
@@ -34,15 +34,12 @@ module.exports = {
     },
     homepage: function(req, res) {
         var dataHomepage = {
-            "status": 1,
-            "msg": "xxxx",
-            "data": {
-                "pro_id": "pro029292",
-                "start_time": 14556585,
-                "end_time": 1450444784,
-                "contraction_time": 14123232,
-                "break_time": 1434234243,
-                "response": []
+            status: 1,
+            msg: "xxxx",
+            data: {
+                tinhot: [],
+                tinnoibat: [],
+                tindiaphuong: []
             }
         };
         Item.find({ isHot: 1 }).sort('createdTime').limit(5).exec(function(err, data) {
@@ -50,21 +47,21 @@ module.exports = {
                 console.log('Error in get all from database', err);
                 res.send(err);
             }
-            dataHomepage.data.response[0] = data;
+            dataHomepage.data.tinhot = data;
         });
         Item.find({ isHot: 2 }).sort('createdTime').limit(5).exec(function(err, data) {
             if (err) {
                 console.log('Error in get all from database', err);
                 res.send(err);
             }
-            dataHomepage.data.response[1] = data;
+            dataHomepage.data.tinnoibat = data;
         });
         itemByRegion.find({ isHot: 2 }).sort('createdTime').limit(5).exec(function(err, data) {
             if (err) {
                 console.log('Error in get all from database', err);
                 res.send(err);
             }
-            dataHomepage.data.response[2] = data;
+            dataHomepage.data.tindiaphuong = data;
         });
         setTimeout(() => {
             res.json(dataHomepage);
