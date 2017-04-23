eaff19eaa073c70c7dcb7d9f8b7f6fc6d90e2a90
MinhTri Ngo <minhtringo141@gmail.com>
2017-04-23 22:53:04 +0700

    fix callback

--- api/items/item.controller.js
+++ api/items/item.controller.js
@@ -37,9 +37,9 @@ module.exports = {
             status: 1,
             msg: "xxxx",
             data: {
-                tinhot: [],
-                tinnoibat: [],
-                tindiaphuong: []
+                hotNews: [],
+                highlights: [],
+                newsByRegion: []
             }
         };
         Item.find({ isHot: 1 }).sort('createdTime').limit(5).exec(function(err, data) {
@@ -47,24 +47,22 @@ module.exports = {
                 console.log('Error in get all from database', err);
                 res.send(err);
             }
-            dataHomepage.data.tinhot = data;
+            dataHomepage.data.hotNews = data;
+            Item.find({ isHot: 2 }).sort('createdTime').limit(5).exec(function(err, data) {
+                if (err) {
+                    console.log('Error in get all from database', err);
+                    res.send(err);
+                }
+                dataHomepage.data.highlights = data;
+                itemByRegion.find().sort('createdTime').limit(5).exec(function(err, data) {
+                    if (err) {
+                        console.log('Error in get all from database', err);
+                        res.send(err);
+                    }
+                    dataHomepage.data.newsByRegion = data;
+                    res.json(dataHomepage);
+                });
+            });
         });
-        Item.find({ isHot: 2 }).sort('createdTime').limit(5).exec(function(err, data) {
-            if (err) {
-                console.log('Error in get all from database', err);
-                res.send(err);
-            }
-            dataHomepage.data.tinnoibat = data;
-        });
-        itemByRegion.find({ isHot: 2 }).sort('createdTime').limit(5).exec(function(err, data) {
-            if (err) {
-                console.log('Error in get all from database', err);
-                res.send(err);
-            }
-            dataHomepage.data.tindiaphuong = data;
-        });
-        setTimeout(() => {
-            res.json(dataHomepage);
-        }, 2000);
     }
 }
